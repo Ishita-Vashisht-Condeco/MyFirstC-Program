1887a6d97a291034670f7caad29af072c47ba979
Ishita Vashisht <Ishita.Vashisht@condecosoftware.com>
2022-10-03 18:17:16 +0530

    first commit

---
+++ program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+//using WeatherUtilities = MyUtilities.WeatherUtilities;
+using MyUtilities;
+
+
+namespace MyFirstProgram
+{
+
+    class CheckComfort
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("where should we go in may");
+            WeatherUtilities.Report("san francisco", WeatherUtilities.FahrenheitToCelsius(65),73);
+            WeatherUtilities.Report("denver", WeatherUtilities.FahrenheitToCelsius(77), 55);
+            WeatherUtilities.Report("bologna", 23, 65);
+        }
+    }
+}
